f693a4045620fe2862f01a16ba4b0b991c0c0439
Inês Filipe <ines.filipe@tecnico.ulisboa.pt>
2018-11-08 15:04:36 +0000

    Added test to match example against regex PCT-6 #resolve

--- src/test/java/org/fenixedu/PostalCodeValidatorTest.java
+++ src/test/java/org/fenixedu/PostalCodeValidatorTest.java
@@ -23,4 +23,13 @@ public class PostalCodeValidatorTest {
         assertEquals(1, PostalCodeValidator.getValidatorMap().size());
     }
 
+    @Test
+    public void testExampleRegex() {
+        PostalCodeValidator.getValidatorMap().values().forEach(p -> {
+            String country = p.get("ISO").getAsString();
+            String example = p.get("Example").getAsString();
+            assertTrue(String.format("Example does not match for country \"%s\"", country), PostalCodeValidator.isValidAreaCode(country, example));
+        });
+    }
+
 }
